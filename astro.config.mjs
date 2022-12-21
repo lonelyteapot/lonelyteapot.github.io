ecc9a3f7aadcd4e1fb645cc6abb5105b060d75ec
Dmitry Semenov <lonelyteapot@gmail.com>
2022-12-21 07:57:15 +0300

    Add old data and support yaml imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from "astro/config";
+import Yaml from "@rollup/plugin-yaml";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [Yaml()],
+    },
+});
