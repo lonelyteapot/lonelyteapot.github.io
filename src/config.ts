98b73ee6bbf77b81a14da85f77f9f86cc24b892a
Dmitry Semenov <lonelyteapot@gmail.com>
2022-12-21 08:06:59 +0300

    Move navigation items to config

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,10 @@ export const SOCIAL_LINKS = [
     "https://github.com/lonelyteapot",
     "https://t.me/lonelyteapot",
 ];
+
+export const NAV_ITEMS = [
+    { title: "About", url: "/#" },
+    { title: "My Skills", url: "/#skills" },
+    { title: "My Software", url: "/#software" },
+    { title: "Contact Me", url: "/#contact-me" },
+];
