fdafee3ef6babf1437dc7f2b53edd6adf30c530f
Dmitry Semenov <lonelyteapot@gmail.com>
2022-12-23 19:36:04 +0300

    Rework site header

--- src/config.ts
+++ src/config.ts
@@ -1,28 +1,7 @@
 export const SITE_TITLE = "Dmitry Semenov | Software Developer";
 export const SITE_DESCRIPTION = "Personal Website";
 
-export const HEADER_TITLE = "Dmitry Semenov";
-export const HEADER_TAGLINE = "Software Developer";
 export const HEADER_USERNAME = "lonelyteapot";
 export const FOOTER_FULLNAME = "Dmitry Semenov";
 
-// Browse icons: https://icon-sets.iconify.design/
-export const SOCIAL_LINKS = [
-  {
-    url: "https://github.com/lonelyteapot",
-    icon: "mdi:github",
-  },
-  {
-    url: "https://t.me/lonelyteapot",
-    icon: "ic:baseline-telegram",
-  },
-];
-
 export const GOOGLE_ANALYTICS_ID = "G-4N4MCBCG3C";
-
-export const NAV_ITEMS = [
-  { title: "About", url: "/#" },
-  { title: "My Skills", url: "/#skills" },
-  { title: "My Software", url: "/#software" },
-  { title: "Contact Me", url: "/#contact-me" },
-];
