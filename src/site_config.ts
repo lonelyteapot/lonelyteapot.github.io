69aa6c8337036514ee90a7768ef653a4bc275fc8
Dmitry Semenov <lonelyteapot@gmail.com>
2023-10-24 04:08:41 +0300

    Migrate to beampipe.io analytics

--- src/site_config.ts
+++ src/site_config.ts
@@ -3,5 +3,3 @@ export const SITE_DESCRIPTION = "Personal Website";
 
 export const HEADER_USERNAME = "lonelyteapot";
 export const FOOTER_FULLNAME = "Dmitry Semenov";
-
-export const GOOGLE_ANALYTICS_ID = "G-4N4MCBCG3C";
