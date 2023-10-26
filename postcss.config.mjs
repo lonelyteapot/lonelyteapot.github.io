1a5109ff43f9436617c8bbccbbaedc65802ba266
Dmitry Semenov <lonelyteapot@gmail.com>
2023-10-26 08:12:11 +0300

    Continue migration to tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
 export default {
   plugins: {
-    "postcss-import": {},
-    "tailwindcss/nesting": {},
-    tailwindcss: {},
-    autoprefixer: {},
+    "postcss-import": true,
+    "tailwindcss/nesting": true,
+    tailwindcss: true,
+    autoprefixer: true,
   },
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#29c785",
+        canvas: "#1f1f1f",
+
+        // "primary-color": "#29c785",
+        // "secondary-color": "#29c785",
+        // "background-color": "#222222",
+        // "background-color-opaque": "#222222ee",
+        // "canvas-color": "#222222",
+        // "color-on-primary": "#232729",
+        // "color-on-secondary": "#ffffff",
+        // "color-on-background": "#ffffff",
+        // "color-on-canvas": "#ffffff",
+      },
+      screens: {
+        hbp: "480px",
+      },
+      fontFamily: {
+        primary: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        secondary: ["Work Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
